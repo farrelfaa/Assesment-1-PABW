faaacb233aeba33ea23d82d10becebee99cc97c2
farrelfaa <farrelfadhilah1@gmail.com>
2024-04-05 14:52:38 +0700

    versi 1.2

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-// let buku = [
-//     {"nama": "sherlock", "jumlah":2},
-//     {"nama": "lupin", "jumlah":2},
-//     {"nama": "aladin", "jumlah":2},
-//   ]
+let buku = [
+    {"nama": "sherlock", "jumlah":2},
+    {"nama": "lupin", "jumlah":2},
+    {"nama": "aladin", "jumlah":2},
+  ]
 
-// function pinjam() {
-//     let jumlah = document.getElementById('jumlah').value;
-//     let selectedbuku = document.getElementById('buku').value;
-//     let found = false;
+function pinjam() {
+    let jumlah = document.getElementById('jumlah').value;
+    let selectedbuku = document.getElementById('buku').value;
+    let found = false;
 
 
-//     if (jumlah === "") {
-//         alert("masukkan jumlah buku");
-//         return;
-//     }
+    if (jumlah === "") {
+        alert("masukkan jumlah buku");
+        return;
+    }
 
-//     for (let i = 0; i < buku.length; i++) {
-//         if (buku[i].nama === selectedbuku) {
-//             found = true;
-//             if (parseInt(jumlah) > buku [i].jumlah){
-//                 alert('jumlah buku tidak tersedia ');
-//             } 
-//             else {
-//                 alert('silahkan ambil buku anda');
-//             }
-//         }
-//     }
+    for (let i = 0; i < buku.length; i++) {
+        if (buku[i].nama === selectedbuku) {
+            found = true;
+            if (parseInt(jumlah) > buku [i].jumlah){
+                alert('jumlah buku tidak tersedia ');
+            } 
+            else {
+                alert('silahkan ambil buku anda');
+            }
+        }
+    }
 
-//     if (!found) {
-//         alert('buku tidak ditemukan');
-//     }
-// }
+    if (!found) {
+        alert('buku tidak ditemukan');
+    }
+}
